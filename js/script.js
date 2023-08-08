3103c391c37e2974867b3804450ab27b3bceecc5
kxiside <swendside@gmail.com>
2023-08-08 12:46:16 -0700

    adding for day 2

--- js/script.js
+++ js/script.js
@@ -1,50 +1,171 @@
 	/*----- constants -----*/
-// number of stars(mines)
-const starMines = 10
 
-	/*----- state variables -----*/
-// number of cells in each column & row
-let grid = 9
-let visible = false
-let flags = 10
-let stars = starMines
+// setting rows & columns amount
+const rows = 9
+const columns = 9
+
+// how many times a cell was clicked
+const cellClicked = 0
+
+// board set to 0 on initialization
+const board = []
+
     
-        /*----- cached elements  -----*/
+    /*----- state variables -----*/
+
+// cell disabled until ship is clicked
+let shipSet = false
+
+// number of stars(mines)
+let stars = 10
+
+// stars position on board
+let starsCoordinate = []
+
+// solar system destroyed
+let system404 = false;
+
+         
+    /*----- cached elements  -----*/
+
 // selects html element with class name of board and assisgns it 
-const board = document.querySelector('.board')
- // selects all cells
-const cells = document.querySelectorAll('.cell')
+const boardEl = document.querySelector('#board')
+
+// connectiong Id of stars count to new starsEl element
+starsEl = document.getElementById("starsCount")
+
+// listening for cell click
+
     
     
     
         /*----- functions -----*/
 
-
-// initialize board variables
+// initialize board 
 function init () {
+
+    // outputing stars count to html
+    starsEl.innerText = stars
+
+    //render all game logic on initialization
     render()
 }
+
+// creating the board
 function renderBoard() {
-    // iterates 81 cells
-    for(let i = 0; i < grid * grid; i++) {
-    // new div element assigned to const cell
-    const cell = document.createElement('div')
-    // classname of 'cell is is set for each div
-    cell.className = 'cell'
-    // each div iteration will be given a text number 1 - 81
-    cell.textContent = i + 1
-    // cell is appended to board creating a child element for each iteration of cells
-    board.appendChild(cell)
+
+    // row for loop to create the array of rows
+    for (let r = 0; r < rows; r++) {
+
+        // empty array staged for push
+        let row = []
+
+        // column for loop 
+        for (let c = 0; c < columns; c++) {
+
+            // creating cell element to hold divs
+            let cell = document.createElement('div')
+            
+            
+            // creating cell ID of rows & columns
+            cell.id = (`${r}-${c}`)
+
+            //creating cell divs appended to the new board element
+            boardEl.append(cell)
+
+            // pushing cell string to row array
+            row.push(cell)
+
+            
+        }
+        // new row array being pushed to board array
+        board.push(row)
+    }  
+}
+
+
+// render ship markers enabled or disabled
+function renderShip() {
+
+    // setting ship background color depending on true or false
+    shipSet = !shipSet;
+
+    // ship marker is tied to html element "ship"
+    // if ship button is darkgray its set on otherwise its off
+    document.getElementById("ship").style.backgroundColor = shipSet ? "darkgray" : "lightgrey";
+  }
+
+
+
+// setting stars to random cells on start
+function renderStars() {
+
+    
+    // loop through stars value
+    for (let i = 0; i < 10; i++) {
+
+        // create variable for random rows & columns
+        let starPosition = randomStar(rows, columns)
+
+        // randomizing star position ['0-0']
+        let id = starPosition.row.toString() + "-" + starPosition.column.toString()
+
+        // if theres no ID push in new ID
+        if (!starsCoordinate.includes(id)) {
+            starsCoordinate.push(id)
+
+           // minus stars count by 1
+           stars--
+        }
     }
+    
+}
+
+// random row & column function
+function randomStar(rows, columns) {
+    let column = Math.floor(Math.random() * columns)
+    let row = Math.floor(Math.random() * rows)
+    return {row, column}
+}
+
+// showing stars on board
+function showStars() {
+    
+    // loop board rows
+    board.forEach(row => {
+
+        // loop row cells
+        row.forEach(cell => {
+
+            // push star into cell of random star coordinate
+            if (starsCoordinate.includes(cell.id)) {
+                
+                // stylize cell when revealed
+                cell.innerText = "star"
+                cell.style.backgroundColor = "slategrey"
+            }
+        })
+    })
 }
 
 
 
-// render game on refresh
+
+
+// render game 
 function render() {
     renderBoard()
+    renderStars()
+    renderShip()
+    
 }
     
         /*----- event listeners -----*/
+// adding a click function to the board element
 document.addEventListener('click', board)
+
+// setting the ship button to activate or deactivate on click
+document.getElementById('ship').addEventListener('click', renderShip)
+
+// loading game on initialize
 document.addEventListener('DOMContentLoaded', init)
